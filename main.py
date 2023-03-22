f1c76a82b35fe0548653f00ad4e41e8fc6b82393
Joshua Niewiadomski <joshua.niewiadomski@me.com>
2023-03-21 21:04:28 -0500

    dummy data update

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from dash_bootstrap_components.themes import LUX
 from src.components.layout import create_layout
 from src.data.loader import load_transaction_data, load_additional_data, merge
 
-DATA_PATH = "https://raw.githubusercontent.com/JoshuaN87/dash-app/main/data/studiosalesdataResults.csv"
-DATA_PATH_2 = "https://raw.githubusercontent.com/JoshuaN87/dash-app/main/data/web_sales_combined.csv"
+#DATA_PATH = "https://raw.githubusercontent.com/JoshuaN87/dash-app/main/data/studiosalesdataResults.csv"
+#DATA_PATH_2 = "https://raw.githubusercontent.com/JoshuaN87/dash-app/main/data/web_sales_combined.csv"
 
 #loading data and creating the data manager
 data = load_transaction_data(DATA_PATH)
